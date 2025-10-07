bc0dee7ed52dd2ca09af85f9882c8746cd6a367c
Elin Simenstad <elin_sim@hotmail.com>
2025-10-07 13:58:26 +0200

    Add new routes for collection, artifacts, and plushies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import linksRouter from "./links.js";
 import usersRouter from "./users.js";
+import collectionRouter from "./collection.js";
+import linksRouter from "./links.js";
+import artifactsRouter from "./artifacts.js";
+import plushRouter from "./plushies.js";
 
 const apiRouter = Router();
 
 // Mount individual routers under their respective paths
-apiRouter.use("/links", linksRouter); // Matches /palia/links...
-apiRouter.use("/users", usersRouter); // Matches /palia/users...
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/collection", collectionRouter);
+
+apiRouter.use("/links", linksRouter);
+apiRouter.use("/artifacts", artifactsRouter);
+apiRouter.use("/plushies", plushRouter);
 
 export default apiRouter;

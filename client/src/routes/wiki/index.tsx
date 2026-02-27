a7ee95f1dbc4c81bc4288e037e1eb03c31f4e3d4
Elin Simenstad <elin_sim@hotmail.com>
2026-02-27 10:17:12 +0100

    Redirect from /wiki/artifacts to /wiki/bugs

--- client/src/routes/wiki/index.tsx
+++ client/src/routes/wiki/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/wiki/")({
-  beforeLoad: () => {
-    throw redirect({
-      to: "/wiki/artifacts",
-    });
-  },
+    beforeLoad: () => {
+        throw redirect({
+            to: "/wiki/bugs",
+        });
+    },
 });

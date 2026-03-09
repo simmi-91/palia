f937ee362ac93203aa34d6a81ea5fde3d0df9e4d
Elin Simenstad <elin_sim@hotmail.com>
2026-03-09 10:00:49 +0100

    Remove archived routes and associated pages.

--- client/src/routes/archive/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/archive/")({
-    beforeLoad: () => {
-        throw redirect({
-            to: "/wiki/bugs",
-        });
-    },
-});
